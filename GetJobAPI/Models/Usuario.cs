68ce4d440e18740a8515dea9f9eb512e650642d6
Sergitxin22 <sergiohablador@gmail.com>
2023-02-18 23:48:22 +0100

    Create Usuario.cs

---
+++ GetJobAPI/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetJobAPI.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Contraseña { get; set; }
+        public string Email { get; set; }
+        public string NumeroTelefono { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
