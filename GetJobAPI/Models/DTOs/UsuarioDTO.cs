ee43d8c31a62d2ebdb5828cd3ad4839885b04f7d
Sergitxin22 <sergiohablador@gmail.com>
2023-02-18 23:48:26 +0100

    Create UsuarioDTO.cs

---
+++ GetJobAPI/Models/DTOs/UsuarioDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetJobAPI.Models
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Usuario { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+        public string NumeroTelefono { get; set; }
+    }
+}
